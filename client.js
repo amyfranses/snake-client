1f9f81c12083b19a09208e81030ec61b1ba6b3e8
amyfranses <amyfranses@gmail.com>
2022-02-21 20:37:33 +0000

    add play and client files

---
+++ client.js
@@ -0,0 +1,19 @@
+// establishes a connection with the game server
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", (data) => {
+    console.log(data);
+    // code that does something when the connection is first established
+  });
+  return conn;
+};
+module.exports = { connect };
